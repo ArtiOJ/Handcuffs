1c2969aadc3c3236d0492654dcd015d9b3e15fb7
ArtiOJ <jamespodom@gmail.com>
2026-01-18 04:25:22 +0900

    added command to get cuffs

--- src/main/java/dev/artifabrian/handcuffs/commands/GetHandcuffsCommand.java
+++ src/main/java/dev/artifabrian/handcuffs/commands/GetHandcuffsCommand.java
@@ -1,4 +1,31 @@
 package dev.artifabrian.handcuffs.commands;
 
-public class GetHandcuffsCommand {
+import dev.artifabrian.handcuffs.Handcuffs;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
+
+public class GetHandcuffsCommand implements CommandExecutor {
+    private Handcuffs plugin;
+
+    public GetHandcuffsCommand(Handcuffs plugin) {
+        this.plugin = plugin;
+
+        plugin.getCommand("gethandcuffs").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        if (args.length == 0) {
+            Player player = (Player) sender;
+            Inventory inventory = player.getInventory();
+            inventory.addItem(plugin.getNewCuff());
+            return true;
+        }
+        return false;
+    }
 }
