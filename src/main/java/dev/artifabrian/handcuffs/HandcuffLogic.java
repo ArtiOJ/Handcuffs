18748dbd28f2232c6d977fdf62239d2466a251d7
artifabrian <jamespodom@gmail.com>
2026-01-17 17:27:12 -0500

    Progress update

--- src/main/java/dev/artifabrian/handcuffs/HandcuffLogic.java
+++ src/main/java/dev/artifabrian/handcuffs/HandcuffLogic.java
@@ -10,17 +10,28 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scheduler.BukkitTask;
 
-import java.util.Objects;
+import java.util.*;
 
 
 public class HandcuffLogic implements Listener {
 
     private Handcuffs plugin;
+    private Map<Player, Player> cuffedPlayersMap = new HashMap<>();
+    private BukkitScheduler scheduler;
+    private BukkitTask task;
+
 
     public HandcuffLogic(Handcuffs plugin) {
         this.plugin = plugin;
+        scheduler = Bukkit.getScheduler();
+        task = scheduler.runTaskTimerAsynchronously(plugin, () -> {
+
+
+
+        }, 0, 1L);
 
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
@@ -36,9 +47,22 @@ public class HandcuffLogic implements Listener {
         if (!item.getType().equals(Material.BLAZE_ROD) && !item.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(plugin, "cuff_item"))) return;
         if (!(event.getRightClicked() instanceof Player interactedPlayer)) return;
 
+        if (cuffedPlayersMap.containsKey(player) && Objects.equals(cuffedPlayersMap.get(player), interactedPlayer)) {
+            Bukkit.broadcastMessage("Already Contained!");
+        }
+        if (cuffedPlayersMap.containsKey(player)) {
+            player.sendMessage(Colorize.format("&cYou cannot handcuff mutable players at a time!"));
+            return;
+        }
+        if (cuffedPlayersMap.containsValue(interactedPlayer)) {
+            player.sendMessage("&cThis player is already handcuffed!");
+            return;
+        }
 
+
+        cuffedPlayersMap.put(player, interactedPlayer);
         // Cuff logic here
-        Bukkit.broadcastMessage(Colorize.format("&cCUFF EM!"));
+
     }
 
 }
