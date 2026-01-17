69cba0724e5ca6c1001e66bfee0e6e4091e4fbda
ArtiOJ <jamespodom@gmail.com>
2026-01-18 04:14:10 +0900

    added test code for creating cuff item

--- src/main/java/dev/artifabrian/handcuffs/Handcuffs.java
+++ src/main/java/dev/artifabrian/handcuffs/Handcuffs.java
@@ -1,7 +1,16 @@
 package dev.artifabrian.handcuffs;
 
-import org.bukkit.configuration.file.YamlConfiguration;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.intellij.lang.annotations.JdkConstants;
+
+import java.util.List;
 
 public final class Handcuffs extends JavaPlugin {
 
@@ -17,5 +26,16 @@ public final class Handcuffs extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public ItemStack getNewCuff() {
+        ItemStack item = new ItemStack(Material.BLAZE_ROD);
+        ItemMeta meta = item.getItemMeta();
+        NamespacedKey namespacedKey = new NamespacedKey(this, "cuff_item");
+        meta.getPersistentDataContainer().set(namespacedKey, PersistentDataType.BOOLEAN, true);
+        meta.displayName(Component.text("Temporary Cuff Item"));
+        meta.lore(List.of(Component.text("Temporary Lore Text")));
+        item.setItemMeta(meta);
+        return item;
     }
 }
---
+++ src/main/java/dev/artifabrian/handcuffs/commands/GetHandcuffsCommand.java
@@ -0,0 +1,4 @@
+package dev.artifabrian.handcuffs.commands;
+
+public class GetHandcuffsCommand {
+}
